54cceece3d2de6e0a161a8480af18178226fcb41
Enzo SP <enzosalatinopicoli@gmail.com>
2026-04-16 01:26:05 +0000

    planning on the project structure

---
+++ context comparison/context_comparison.c
@@ -0,0 +1,9 @@
+#include <stdio.h>;
+//#include "proc_exec.h"
+//#include "thread_exec.h"
+
+void start(int counter){
+    //start processes
+    //start threads
+    //return strings or timer or whatever
+}
---
+++ context comparison/thread context/thread_context.c
@@ -0,0 +1,26 @@
+//body de threads ?
+/**
+* struct threads_body(){
+~~ int* pid_t[];
+~int execution_time[];
+~int cont;
+}
+*/
+
+// a quantidade de threads nn eh fixa
+void thread_run(int qtd){
+    //caso1
+    //threads_body body1;
+    //caso2
+    //threads_body body2;
+    //caso 3
+    //threads_body body3;
+}
+
+
+int threads_run(){
+    //instancia o threads
+    //forqueia uma quantidade de vezes
+    //comeca o contador
+return 0;
+}
